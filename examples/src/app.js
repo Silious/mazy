593ba7dc3595f1b0f476bf9f0e853bf6285465eb
Silious <silious@163.com>
2019-01-09 22:51:12 +0800

    first commit

---
+++ examples/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'mazy/style/component.css';// eslint-disable-line
+import Block from 'mazy/src'; // eslint-disable-line
+
+ReactDOM.render(
+  <div className="component-template">
+    <Block />
+  </div>,
+  document.getElementById('example'),
+);
